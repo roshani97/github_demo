7bc40196abaff548f2f041d0061aefe9802207bb
Roshani K.Chincholkar <43722723+roshani97@users.noreply.github.com>
2021-03-04 11:33:38 +0530

    math.py is added

---
+++ math.py
@@ -0,0 +1,18 @@
+# Add implementation
+def add(x,y):
+	return x+y
+
+# Subtract implementation
+def subtract(x,y):
+    return x-y
+
+# Multiply implementation
+def multiply(x,y):
+	return x*y
+
+# Divide implementation
+def divide(x,y):
+	if(x==0)
+		return 0;
+	else
+		return x/y
